6c115aadde50494fd584df4d7dc9db2afcaef439
ArtyomLavrishchev <temalggt540@gmail.com>
2021-02-15 03:57:42 +0300

    update

--- index.js
+++ index.js
@@ -7,22 +7,10 @@ const port = process.env.PORT || 3010
 const login = process.env.SMTP_LOGIN ||"---"
 const password = process.env.SMTP_PASSWORD ||"---"
 app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
-
-app.use(function(req, res, next){
-    let whitelist = ['localhost:4000', 'localhost:3010', 'https://smtp-nodejs-server01.herokuapp.com/']
-    let host = req.get('host');
-
-    whitelist.forEach(function(val, key){
-        if (host.indexOf(val) > -1){
-            res.setHeader('Access-Control-Allow-Origin', host);
-        }
-    })
-
-    next();
-});
-
 app.use(cors())
+app.options('*', cors());
+
+app.use(bodyParser.json())
 
 let transporter = nodemailer.createTransport({
     service: "gmail",
